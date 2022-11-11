07557fca03cf1e83f9019e26a8d11448b85fa320
JMerca14 <jcmercado@student.fullsail.edu>
2022-11-11 15:05:25 -0500

    Initial Commit

---
+++ PaycheckApp/PaycheckAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaycheckAppApp.swift
+//  PaycheckApp
+//
+//  Created by Juan Mercado on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PaycheckAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
